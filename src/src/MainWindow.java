62a89cceb4a8ca03c56a41ccd22d2f794bb65773
Demeny Zoltan <demenyz28@gmail.com>
2022-05-03 18:42:47 +0200

    Created the base of login form

---
+++ src/src/MainWindow.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class MainWindow {
+    private JTextField fieldMail;
+    private JPasswordField fieldPwd;
+    private JButton buttonLogin;
+    private JButton buttonRegister;
+}
