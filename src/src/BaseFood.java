8997a5bd7747c6d827fe6c7a7b9c7bf0a9708506
Bozsokaroly <karcsivokxd123@gmail.com>
2022-05-05 22:04:31 +0200

    Create BaseFood.java
    Created the base of the food system.

---
+++ src/src/BaseFood.java
@@ -0,0 +1,31 @@
+public class BaseFood implements Cloneable {
+    private static int ID = 1;
+
+    public BaseFood() { setId(BaseFood.ID++) ;}
+    private int id;
+    private double price;
+    private double calories;
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id=id;}
+
+    public double getPrice() {return price;}
+    public void setPrice(double price) {this.price = price;}
+
+    public double getCalories() {return calories;}
+    public void setCalories(double calories) {this.calories = calories;}
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("Id: %d; Price: %p; Calories: %c;",
+                id,
+                price,
+                calories);
+    }
+}
